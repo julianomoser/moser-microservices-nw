1679ad8d9a26e7a8996496e182c48416c1f22296
Juliano Moser <juliano.moserjj@gmail.com>
2022-12-20 01:11:01 -0300

    Criação do projeto

---
+++ src/main/java/br/com/moser/MoserMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package br.com.moser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoserMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoserMicroservicesApplication.class, args);
+    }
+
+}
